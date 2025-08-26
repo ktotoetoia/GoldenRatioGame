3bbe37f19c64eb9badab39dfb722a3bca2917971
Illia Muzyka <75931710+ktotoetoia@users.noreply.github.com>
2025-08-25 21:16:50 -0300

    upd: add state machine

---
+++ GoldenRatioGame/Assets/Test/LegacyPlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using IM.StateMachines;
+using TD.Movement;
+using UnityEngine;
+
+namespace Tests
+{
+    public class LegacyPlayerInput : MonoBehaviour
+    {
+        private IMoveInVector _movement;
+        private IStateMachine _stateMachine;
+
+        private void Awake()
+        {
+            _movement = GetComponent<IMoveInVector>();
+            _stateMachine = new StateMachine(new MovementState(_movement, () => new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))));
+        }
+
+        private void Update()
+        {
+            _stateMachine.Update();
+        }
+
+        private void FixedUpdate()
+        {
+            _stateMachine.FixedUpdate();
+        }
+    }
+}
