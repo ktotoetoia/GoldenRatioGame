05661f8d6a0f11a29219b395603ea3bfb1df0168
Illia Muzyka <75931710+ktotoetoia@users.noreply.github.com>
2025-08-28 19:20:19 -0300

    feat; add selection for graph

---
+++ GoldenRatioGame/Assets/Scripts/ModuleEditor/Selection/GraphSelection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using IM.Graphs;
+using IM.SelectionSystem;
+using UnityEngine;
+
+namespace IM.ModuleEditor
+{
+    public class GraphSelection : ISelector
+    {
+        public event Action<ISelection<object>> OnSelectionUpdated;
+        public ISelectionProvider SelectionProvider { get; }
+
+        private ISelection<object> _current = new Selection<object>();
+
+        public GraphSelection(IModuleGraph graph)
+        {
+            SelectionProvider = new ModuleGraphSelectionProvider(graph);
+        }
+
+        public void UpdateSelectionAt(Vector3 position)
+        {
+            _current = SelectionProvider.SelectAt<object>(position);
+            OnSelectionUpdated?.Invoke(_current);
+        }
+
+        public void UpdateSelectionWithin(Bounds bounds)
+        {
+            _current = SelectionProvider.SelectWithin<object>(bounds);
+            OnSelectionUpdated?.Invoke(_current);
+        }
+
+        public ISelection<T> GetSelection<T>() where T : class
+        {
+            return _current.OfType<T>();
+        }
+    }
+}
