e4d0caa0ca8af717219f842315070b018b158584
Illia Muzyka <75931710+ktotoetoia@users.noreply.github.com>
2025-09-01 15:11:04 -0300

    feat: added readonly struct CappedValueReadonly

---
+++ GoldenRatioGame/Assets/Scripts/Economy/CappedValues/CappedValueReadOnly.cs
@@ -0,0 +1,16 @@
+﻿namespace IM.Economy
+{
+    public readonly struct CappedValueReadOnly<T> : ICappedValueReadOnly<T>
+    {
+        public T MinValue { get; }
+        public T MaxValue { get; }
+        public T Value { get; }
+
+        public CappedValueReadOnly(T value, T minValue, T maxValue)
+        {
+            Value = value;
+            MinValue = minValue;
+            MaxValue = maxValue;
+        }
+    }
+}
