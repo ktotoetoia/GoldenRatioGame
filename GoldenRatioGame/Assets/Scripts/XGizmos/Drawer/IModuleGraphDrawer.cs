2c82789ef74273e3e5b398f631dada1520c3cf22
Illia Muzyka <75931710+ktotoetoia@users.noreply.github.com>
2025-10-19 12:28:49 -0300

    remove XGizmos system

--- GoldenRatioGame/Assets/Scripts/XGizmos/Drawer/IModuleGraphDrawer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace IM.ModuleGraphGizmosDebug
-{
-    public interface IModuleGraphDrawer
-    {
-        IEnumerable<IModuleVisualWrapper> Modules { get; }
-    }
-}
